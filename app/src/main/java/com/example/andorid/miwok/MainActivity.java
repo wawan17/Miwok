83c6af89ee792076e0abb1aac5c10d9e78ac834d
wawan17 <setiawan_wawan17@yahoo.com>
2016-09-27 23:47:53 +0700

    add comment to unused import

--- app/src/main/java/com/example/andorid/miwok/MainActivity.java
+++ app/src/main/java/com/example/andorid/miwok/MainActivity.java
@@ -4,8 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
+//import android.widget.TextView;
+//import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
